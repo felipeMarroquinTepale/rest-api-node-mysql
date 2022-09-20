c12cfd895e48258ea2e0005e7b1ef23ecbc4320c
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2022-09-20 18:15:51 -0500

    primer commit

---
+++ src/database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'datamin3',
+    database:'schoolfmt',
+};
+
+const conn = mysql.createConnection(config);
+
+conn.connect(function(err) {
+    if (err) {
+        console.log('¡Ups!... parece que algo salio mal :c')
+        throw err;
+    }else{
+        console.log('Conexión a la base de datos exitosa!');
+    }
+});
+
+module.exports = conn;
